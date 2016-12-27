806692d8c01b1da4904b336539e35b215d33092c
kryp7ik <cameronyoung@krypticpixel.com>
2016-12-27 09:51:28 -0500

    Minor updates with touch screen to work with label printing

--- resources/views/backend/store/touch/touch.blade.php
+++ resources/views/backend/store/touch/touch.blade.php
@@ -24,12 +24,12 @@
                     <tr v-for="liquid in liquids">
                         <td><button v-on:click="completeOrder( liquid.id )" class="btn btn-info btn-lg">Complete</button></td>
                         <td v-on:click="displayRecipe( liquid )">@{{ liquid.extra ? liquid.recipe + ' XTRA' : liquid.recipe }} </td>
-                        <td v-on:click="displayRecipe( liquid )">@{{ liquid.size + 'ml' }}</td>
+                        <td v-on:click="displayRecipe( liquid )">@{{ liquid.size + ' ml' }}</td>
                         <td v-on:click="displayRecipe( liquid )">@{{ mentholDisplay[liquid.menthol] }}</td>
-                        <td v-on:click="displayRecipe( liquid )">@{{ iceToAdd( liquid ) + 'ml' }}</td>
-                        <td v-on:click="displayRecipe( liquid )">@{{ liquid.nicotine }}mg Add: @{{ nicotineToAdd( liquid ) }}ml</td>
+                        <td v-on:click="displayRecipe( liquid )">@{{ iceToAdd( liquid ) + ' ml' }}</td>
+                        <td v-on:click="displayRecipe( liquid )">"@{{ liquid.nicotine }}" mg Add: @{{ nicotineToAdd( liquid ) }} ml</td>
                         <td v-on:click="displayRecipe( liquid )">@{{ liquid.vg == 100 ? 'MAX' : liquid.vg + '%' }}</td>
-                        <td v-on:click="displayRecipe( liquid )">@{{ addPremix( liquid ) }}ml</td>
+                        <td v-on:click="displayRecipe( liquid )">@{{ addPremix( liquid ) }} ml</td>
                     </tr>
                 </tbody>
             </table>
